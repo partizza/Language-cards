34a37c65904f5375d430e837ff4c5ea4d6f5b53b
partizza <bufferdp@ukr.net>
2018-09-11 22:31:52 +0300

    multivalue cars draft

--- src/main/java/ua/agwebs/lc/SimpleCards.java
+++ src/main/java/ua/agwebs/lc/SimpleCards.java
@@ -1,15 +1,15 @@
 package ua.agwebs.lc;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class SimpleCards extends CardBox {
 
+    static final private String LANG_SEPARATOR = "=";
+    static final private String VAL_SEPARATOR = ";";
+
     private File src;
-    private Map<String, String> carts;
+    private Map<String, Set<String>> carts;
 
     public SimpleCards(File src) {
         this.src = src;
@@ -21,8 +21,15 @@ public class SimpleCards extends CardBox {
             carts = new HashMap<>();
             String str = null;
             while ((str = reader.readLine()) != null) {
-                String[] split = str.split("=");
-                carts.put(split[0].trim(), split[1].trim());
+                String[] split = str.split(LANG_SEPARATOR);
+                String[] keys = split[0].split(VAL_SEPARATOR);
+                String[] values = split[1].split(VAL_SEPARATOR);
+                for (String k : keys) {
+                    k = k.trim();
+                    Set<String> set = carts.getOrDefault(k.trim(), new TreeSet<>());
+                    for (String v : values) set.add(v.trim());
+                    carts.put(k, set);
+                }
             }
         } catch (FileNotFoundException e) {
             System.out.println(e);
@@ -35,12 +42,39 @@ public class SimpleCards extends CardBox {
     protected void drill() {
         try (Scanner scanner = new Scanner(System.in)) {
             while (!carts.isEmpty()) {
-                Iterator<Map.Entry<String, String>> iterator = carts.entrySet().iterator();
+                Iterator<Map.Entry<String, Set<String>>> iterator = carts.entrySet().iterator();
                 while (iterator.hasNext()) {
-                    Map.Entry<String, String> next = iterator.next();
-                    System.out.println(next.getKey());
+                    System.out.println();
+
+                    Map.Entry<String, Set<String>> next = iterator.next();
+                    int size = 0;
+                    System.out.println(next.getKey() + ((size = next.getValue().size()) > 1 ? " (" + size + ") " : ""));
                     String input = scanner.nextLine();
-                    if (input.equals(next.getValue())) iterator.remove();
+
+                    String[] inputs = input.split(VAL_SEPARATOR);
+                    if (inputs.length < size) {
+                        System.out.println("NOT ALL VALUES !!!");
+                        continue;
+                    }
+                    if (inputs.length > size) {
+                        System.out.println("TOO MUCH VALUES !!!");
+                        continue;
+                    }
+
+                    boolean isCorrect = true;
+                    for (String ans : input.split(VAL_SEPARATOR)) {
+                        if (next.getValue().contains(ans.trim())) continue;
+                        else {
+                            isCorrect = false;
+                            System.out.println("NOT CORRECT !!!");
+                            break;
+                        }
+                    }
+
+                    if (isCorrect) {
+                        iterator.remove();
+                        System.out.println("CORRECT !!!");
+                    }
                 }
             }
         }
